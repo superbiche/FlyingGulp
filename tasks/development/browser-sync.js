c6daae890e0f041d5056001903f34e374cfa516f
Michel Tomas <mtomas@lmyr.com>
2016-08-25 12:18:43 +0200

    Stop building assets on watch start

--- tasks/development/browser-sync.js
+++ tasks/development/browser-sync.js
@@ -6,6 +6,6 @@ var config = require('../../config').browsersync.development;
 /**
  * Run the build task and start a server with BrowserSync
  */
-gulp.task('browsersync:development', ['build'], () => {
+gulp.task('browsersync:development', () => {
   browsersync(config);
 });
